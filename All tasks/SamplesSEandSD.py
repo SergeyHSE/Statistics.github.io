8e10f2f43b60f2e9c7c0674dd970327902c56f6b
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2023-09-20 23:55:51 +0300

    Update SamplesSEandSD.py

--- All tasks/SamplesSEandSD.py
+++ All tasks/SamplesSEandSD.py
@@ -9,3 +9,14 @@ import matplotlib.pyplot as plt
 from scipy import stats
 import os
 pd.set_option('display.max_columns', None)
+
+path = r"Your path.xlsx"
+path = path.replace('\\', '/')
+
+data = pd.read_excel(path, sheet_name='2')
+data.head()
+data_costs = data['14070']
+data_costs.head()
+data_costs.shape
+data_costs.isnull().any().any()
+(data_costs == 0).sum() #Calculate number of zeros
