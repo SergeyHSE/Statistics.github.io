a63762cbae31b58c6cc3e18a42bf4e8c75493b63
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2023-09-24 14:03:33 +0300

    Create Stratification.py

---
+++ Stratification/Stratification.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 23 22:39:56 2023
+
+@author: SergeyHSE
+
+We are gonna extract colums kike region, revenue and pig farming cost,
+sort them by revenue, remove zeros. We need to find intervals that help us split our data on
+5 parts (stratums or cohorts). 
+Then we need to find mean value for cohorts and population, calculate variation.
+
+
+"""
