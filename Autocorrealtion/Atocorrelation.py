b1ef6893e6c455deafbbe309924001dad2e79745
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2023-10-13 18:23:26 +0300

    Update Atocorrelation.py

--- Autocorrealtion/Atocorrelation.py
+++ Autocorrealtion/Atocorrelation.py
@@ -4,3 +4,8 @@ import numpy as np
 import statsmodels.api as sm
 import statsmodels.tsa.stattools as adfuller
 pd.set_option('display.max_columns', None)
+
+
+path = r".........\Кисломолоная_продукция.xlsx"
+path = path.replace('\\', '/')
+df = pd.read_excel(path, sheet_name='data')
