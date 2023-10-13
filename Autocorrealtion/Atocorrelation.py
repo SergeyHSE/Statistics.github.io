b67f4f63dd2ddee4e5ff748f807d113585162285
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2023-10-13 18:28:01 +0300

    Update Atocorrelation.py

--- Autocorrealtion/Atocorrelation.py
+++ Autocorrealtion/Atocorrelation.py
@@ -9,3 +9,14 @@ pd.set_option('display.max_columns', None)
 path = r".........\Кисломолоная_продукция.xlsx"
 path = path.replace('\\', '/')
 df = pd.read_excel(path, sheet_name='data')
+
+# Check NaN
+df.isna().any()
+
+df.head()
+
+df = df.drop('Удаление автокорреляци', axis=1)
+
+# Fill NaN
+df['Year'].fillna(method='ffill', inplace=True)
+df
