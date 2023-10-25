7125ee87fb1d91bab29ba8d702b4f66fbecc2933
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2023-10-25 19:03:17 +0300

    Update ConfidenceInterval.py

--- ConfidenceInterval/ConfidenceInterval.py
+++ ConfidenceInterval/ConfidenceInterval.py
@@ -14,3 +14,11 @@ def confidence_experiment(theta, sample_size=10, num_samples=20, alpha=0.05):
         # Generate samples
         data = np.random.exponential(scale=1/theta, size=sample_size)
         theta_hat = sample_size / np.sum(data)
+
+        # Asymptotic confidence interval 1
+        se_1 = theta_hat / np.sqrt(sample_size)
+        ci_1 = (theta_hat - stats.norm.ppf(1 - alpha/2) * se_1, theta_hat + stats.norm.ppf(1 - alpha/2) * se_1)
+        
+        # (для theta^2)
+        se_2 = (2 * theta_hat**2) / np.sqrt(sample_size)
+        ci_2 = (theta_hat**2 - stats.norm.ppf(1 - alpha/2) * se_2, theta_hat**2 + stats.norm.ppf(1 - alpha/2) * se_2)
