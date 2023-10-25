e592105811ec895b8df82b0ed20bbf7b429a5cb7
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2023-10-25 18:59:33 +0300

    Update ConfidenceInterval.py

--- ConfidenceInterval/ConfidenceInterval.py
+++ ConfidenceInterval/ConfidenceInterval.py
@@ -1,1 +1,8 @@
+"""
+Let' model a lot of experiments with the same conditions.
+We are gonna generate about 20 samples with size 10 and calculate the borders all of them
+and check H-hypothesis
+"""
 import numpy as np
+import scipy.stats as stats
+import pandas as pd
