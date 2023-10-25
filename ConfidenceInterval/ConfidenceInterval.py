3ed7cbd11718ce67ceae3f508684ecbceeb0cd08
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2023-10-25 19:01:18 +0300

    Update ConfidenceInterval.py

--- ConfidenceInterval/ConfidenceInterval.py
+++ ConfidenceInterval/ConfidenceInterval.py
@@ -6,3 +6,11 @@ and check H-hypothesis
 import numpy as np
 import scipy.stats as stats
 import pandas as pd
+
+def confidence_experiment(theta, sample_size=10, num_samples=20, alpha=0.05):
+    results = []
+    
+    for _ in range(num_samples):
+        # Generate samples
+        data = np.random.exponential(scale=1/theta, size=sample_size)
+        theta_hat = sample_size / np.sum(data)
