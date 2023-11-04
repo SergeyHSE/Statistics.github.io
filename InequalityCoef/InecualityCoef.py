fecd4083c1141c91ce79ad9128438612d6a3e142
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2023-11-04 12:26:42 +0300

    Update InecualityCoef.py

--- InequalityCoef/InecualityCoef.py
+++ InequalityCoef/InecualityCoef.py
@@ -1,3 +1,12 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+pd.set_option('display.max_columns', None)
+
+path = r"C:\Users\User\Documents\книги\ВШЭ\учёба\Статистика\Семинар 5\data (6).xls"
+path = path.replace('\\', '/')
+data = pd.read_excel(path)
+data.head()
+data = data.drop('Unnamed: 1', axis=1)
+data.head()
