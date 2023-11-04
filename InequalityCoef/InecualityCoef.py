2a7aea18bcab8d565b212c928ada44899dbc244e
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2023-11-04 12:29:24 +0300

    Update InecualityCoef.py

--- InequalityCoef/InecualityCoef.py
+++ InequalityCoef/InecualityCoef.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 pd.set_option('display.max_columns', None)
 
-path = r"C:\Users\User\Documents\книги\ВШЭ\учёба\Статистика\Семинар 5\data (6).xls"
+path = r"............\data (6).xls"
 path = path.replace('\\', '/')
 data = pd.read_excel(path)
 data.head()
@@ -30,3 +30,15 @@ def lorenz_curve_gini(data, columnName=None):
         raise ValueError("data should be a DataFrame, list, or NumPy array.")
     
     total_income = sorted_data.sum()
+    cumulative_income = np.cumsum(sorted_data) / total_income
+    cumulative_population = np.arange(1, len(sorted_data) + 1) / len(sorted_data)
+
+    plt.figure(figsize=(8, 8), dpi=90)
+    plt.plot(cumulative_population, cumulative_income, label='Lorenz curve',
+             color='tab:red', linewidth=2.0)
+    plt.plot([0, 1], [0, 1], 'k--', label='Equality Line',
+             linewidth=2.0, color='tab:green')
+    plt.xlabel('Cumulative % of Population', fontsize=14)
+    plt.ylabel(f'Cumulative % of {columnName} Income', fontsize=14)
+    plt.title(f'Lorenz Curve for {columnName}', fontsize=18)
+    
